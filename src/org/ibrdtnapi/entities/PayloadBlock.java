9f5711560214dff00b15dc7fdc443efdd843da80
Maxime de Streel <maxime.destreel@student.uclouvain.be>
2020-02-17 17:47:55 +0100

    Make use of java.util.Base64
    sun.misc is not supported since Java 9, the right class to use is
    java.util.Base64.
    
    +reformat code

--- src/org/ibrdtnapi/entities/PayloadBlock.java
+++ src/org/ibrdtnapi/entities/PayloadBlock.java
@@ -1,67 +1,61 @@
 package org.ibrdtnapi.entities;
 
-import java.io.IOException;
+import java.util.Base64;
 import java.util.logging.Logger;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
 public class PayloadBlock {
-	private static final Logger log = Logger.getLogger(Bundle.class.getName());
-	private String encoded = null;
-	private byte[] decoded = null;
-	
-	public PayloadBlock(String encoded) {
-		this.setEncoded(encoded);
-	}
+    private static final Logger log = Logger.getLogger(Bundle.class.getName());
+    private String encoded = null;
+    private byte[] decoded = null;
 
-	public PayloadBlock(byte[] decoded) {
-		this.setDecoded(decoded);
-	}
+    public PayloadBlock(String encoded) {
+        this.setEncoded(encoded);
+    }
 
-	public PayloadBlock(PayloadBlock payloadBlock) {
-		this(payloadBlock.getEncoded());
-	}
+    public PayloadBlock(byte[] decoded) {
+        this.setDecoded(decoded);
+    }
 
-	public String getEncoded() {
-		return this.encoded;
-	}
+    public PayloadBlock(PayloadBlock payloadBlock) {
+        this(payloadBlock.getEncoded());
+    }
 
-	public byte[] getDecoded() {
-		return this.decoded;
-	}
+    public String getEncoded() {
+        return this.encoded;
+    }
 
-	public int getLength() {
-		return decoded.length;
-	}
+    public void setEncoded(String encoded) {
+        this.encoded = new String(encoded);
+        this.decoded = Base64.getDecoder().decode(this.encoded); // .decodeBuffer(this.encoded);
+    }
 
-	public void setEncoded(String encoded) {
-		this.encoded = new String(encoded);
-		try {
-			this.decoded = new BASE64Decoder().decodeBuffer(this.encoded);
-		} catch (IOException e) {
-			PayloadBlock.log.warning("Could not base64::decode() the payload of the bundle:" + this + ". " + e.getMessage());
-		}
-	}
+    public byte[] getDecoded() {
+        return this.decoded;
+    }
 
-	private void setDecoded(byte[] decoded) {
-		this.decoded = new byte[decoded.length];
-		System.arraycopy(decoded, 0, this.decoded, 0, decoded.length);
-		this.encoded = new String(new BASE64Encoder().encodeBuffer(this.decoded)).trim();
-	}
+    private void setDecoded(byte[] decoded) {
+        this.decoded = new byte[decoded.length];
+        System.arraycopy(decoded, 0, this.decoded, 0, decoded.length);
+        //this.encoded = new String(new BASE64Encoder().encodeBuffer(this.decoded)).trim();
+        this.encoded = new String(Base64.getEncoder().encode(this.decoded)).trim();
+    }
 
-	public String toString(boolean full) {
+    public int getLength() {
+        return decoded.length;
+    }
 
-		StringBuilder str = new StringBuilder();
-		str.append(((this.getLength() == 0) ? "X" : this.getLength()) + ":");
-		str.append("" + new String(this.decoded).trim());
-		if (full) {
-			str.append("#" + this.encoded + "#");
-		}
-		return str.toString();
-	}
+    public String toString(boolean full) {
 
-	public String toString() {
-		return this.toString(false);
-	}
+        StringBuilder str = new StringBuilder();
+        str.append(((this.getLength() == 0) ? "X" : this.getLength()) + ":");
+        str.append("" + new String(this.decoded).trim());
+        if (full) {
+            str.append("#" + this.encoded + "#");
+        }
+        return str.toString();
+    }
+
+    public String toString() {
+        return this.toString(false);
+    }
 }
